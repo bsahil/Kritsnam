4bb48b1216aa07334aa12010383267d221e2791e
bsahil <bsahil012@gmail.com>
2018-07-14 20:34:57 +0530

    updated Turbidity sensor code

--- Turbidity-sensor/calibration.py
+++ Turbidity-sensor/calibration.py
@@ -2,10 +2,13 @@
 import machine
 
 standard=input("Enter standard solution value: ")
-Vcc=input("Enter Vcc Value: ")
-voltage_factor=input("Enter voltage divider factor: ")
+#Vcc=input("Enter Vcc Value: ")
+voltage_factor=input("Enter voltage Multiplying factor: ")
 adc=machine.ADC(machine.Pin(34))
 a=adc.read()
-v=a*float(Vcc)*float(voltage_factor)/4096
+v=a*float(voltage_factor)/4096
 
 factor = int(standard)/v
+
+with open('calib.txt','w') as f:
+    f.write(str(factor))
--- Turbidity-sensor/reading.py
+++ Turbidity-sensor/reading.py
@@ -1,10 +1,15 @@
 import machine
-from calibration import *
+
+with open('calib.txt','r') as f:
+    faq=f.read()
+
+#Vcc=input("Enter Vcc Value: ")
+voltage_factor=input("Enter voltage Multiplying factor: ")
 
 adc1=machine.ADC(machine.Pin(34))
 a1=adc1.read()
 
-v=a1*float(Vcc)/4096
-Turbidity=factor*v
+v=a1*float(voltage_factor)/4096
+Turbidity=float(faq)*v
 
 print("Turbidity value is: "+str(Turbidity))
